8c121443120db3ad3e7d8d1bea21b2aef108965e
Marcelo Slivinski de Matos <marcelosmatos@hotmail.com>
2013-12-19 17:37:59 -0200

    adapted for rails 4

--- lib/delocalize/rails_ext/active_record_rails4.rb
+++ lib/delocalize/rails_ext/active_record_rails4.rb
@@ -12,4 +12,30 @@ ActiveRecord::ConnectionAdapters::Column.class_eval do
   end
 
   alias_method_chain :type_cast, :localization
+  
+  def type_cast_for_write_with_localization(value)
+    if number? && I18n.delocalization_enabled?
+      old_value = value
+      value = Numeric.parse_localized(value)
+      # if type == :integer
+      #   begin
+      #     value = Integer(i) 
+      #   rescue 
+      #     value = old_value 
+      #   end
+      # else
+      #   value = is_float?(value) ? value.to_f : old_value
+      # end
+      value
+    end
+    type_cast_for_write_without_localization(value)
+  end
+
+  alias_method_chain :type_cast_for_write, :localization
+  
+  # def is_float?(fl)
+  #   !!Float(fl) rescue false
+  # end
+  
+
 end
